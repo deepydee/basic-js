9dd8a7764d2810d8ad498d1b217a18fb4dbbcc94
Vitaliy Semenov <synthex@bk.ru>
2023-04-14 18:41:14 +0600

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,118 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.ALPHABET = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+    this.n = this.ALPHABET.length;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  /**
+   *
+   * @param {string} message string to encode
+   * @param {string} key keyword
+   */
+  encrypt(message, key) {
+    if (!message || ! key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encoded = '';
+    let newKey = this.makeKeyEqualToString(message, key);
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.ALPHABET.includes(message[i])) {
+        encoded += message[i];
+      } else {
+        let letterIndex =
+        ( this.ALPHABET.indexOf(message[i]) +
+        this.ALPHABET.indexOf(newKey[i]) ) % this.n;
+
+        let letter = this.ALPHABET[letterIndex];
+
+        encoded += letter
+      }
+    }
+
+    return this.direct
+      ? encoded
+      : [...encoded].reverse().join('');
+  }
+
+  /**
+   *
+   * @param {string} encryptedMessage string to decode
+   * @param {string} key keyword
+   */
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || ! key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let decoded = '';
+    let newKey = this.makeKeyEqualToString(encryptedMessage, key);
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+
+      if (!this.ALPHABET.includes(encryptedMessage[i])) {
+        decoded += encryptedMessage[i];
+      } else {
+        let letterIndex =
+        ( this.ALPHABET.indexOf(encryptedMessage[i]) -
+        this.ALPHABET.indexOf(newKey[i]) ) % this.n;
+
+        if (letterIndex < 0) {
+          letterIndex += this.n;
+        }
+
+        let letter = this.ALPHABET[letterIndex];
+
+        decoded += letter
+      }
+    }
+
+    return this.direct
+      ? decoded
+      : [...decoded].reverse().join('');
+  }
+
+  /**
+   *
+   * @param {string} string
+   * @param {string} key
+   */
+  makeKeyEqualToString(string, key) {
+    let keyLength = key.length;
+    let newKey = '';
+
+    for (let i = 0, cnt = 0; i < string.length; i++) {
+      if (cnt === keyLength) {
+        cnt = 0;
+      }
+
+      if (!this.ALPHABET.includes(string[i])) {
+        newKey += string[i];
+        continue;
+      }
+
+      newKey += key[cnt];
+      cnt++
+    }
+
+    return newKey;
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+
+console.log(directMachine.encrypt('attack at dawn!', 'alphonse'));
+console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'));
+
 
 module.exports = {
   VigenereCipheringMachine
