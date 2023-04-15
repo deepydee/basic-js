1fa6e8b215ec4272c566825234a0e9cc2fdf1554
Vitaliy Semenov <synthex@bk.ru>
2023-04-15 22:16:19 +0600

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (str === undefined) return false;
+
+  str = String(str);
+
+  if (typeof str === 'object') {
+    str = str[Symbol.toPrimitive](hint);
+  }
+
+  let {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator,
+  } = options;
+
+  if (!repeatTimes) return str + addition;
+
+  if (addition !== undefined) {
+    if (typeof addition === 'object' && addition !== null &&  Object.getOwnPropertySymbols(addition).length !== 0) {
+      addition = addition[Symbol.toPrimitive]('string');
+    } else {
+      addition = String(addition);
+    }
+  }
+
+  separator = separator ?? '+';
+  additionSeparator = additionSeparator ?? '|';
+
+  let repeatedString = '';
+  let addStr = '';
+
+  for (let i = 0; i < repeatTimes; i++) {
+    addStr = '';
+
+    if (additionRepeatTimes) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        if (j === additionRepeatTimes - 1) {
+          addStr += addition;
+          break;
+        }
+
+        addStr += addition + additionSeparator;
+      }
+    } else if (addition) {
+      addStr += addition;
+    }
+
+    if (i === repeatTimes - 1) {
+      repeatedString += str + addStr;
+      break;
+    }
+
+    repeatedString += str + addStr + separator;
+  }
+
+  return repeatedString;
 }
+
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+
+console.log( repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }) );
 
 module.exports = {
   repeater
