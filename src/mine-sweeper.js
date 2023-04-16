eec9ca8f5abdbde6f5e7dddf11288fde28d18411
Vitaliy Semenov <synthex@bk.ru>
2023-04-16 22:29:09 +0600

    feat: add mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,9 +1,9 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * In the popular Minesweeper game you have a board with some mines and those cells
+ * In the popular Minesweeper game you have a board with some mines and those cols
  * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
+ * in the neighboring cols. Starting off with some arrangement of mines
  * we want to create a Minesweeper game setup.
  *
  * @param {Array<Array>} matrix
@@ -23,10 +23,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const resultMatrix = matrix.map(row => row.map(el => 0));
+
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  for (let rowIdx = 0; rowIdx < rows; rowIdx++) {
+    for (let colIdx = 0; colIdx < cols; colIdx++) {
+
+      if (matrix[rowIdx][colIdx]) {
+        for (let ri = rowIdx - 1; ri <= rowIdx + 1; ri++) {
+          if (ri > -1) {
+            for (let ci = colIdx - 1; ci <= colIdx + 1; ci++) {
+              if (ci > -1 && !(ri === rowIdx && ci === colIdx)) {
+                resultMatrix[ri][ci]++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return resultMatrix;
 }
+
+// const matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false]
+// ];
+
+/*(i) 0 1 2 (j)
+ * 0 [1 2 1]
+ * 1 [2 1 1]
+ * 2 [1 1 1]
+ */
+
+// console.log(minesweeper(matrix));
 
 module.exports = {
   minesweeper
